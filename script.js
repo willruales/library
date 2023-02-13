0fb2bb414b58f334a66eed6fc69ebcedfd5c79e6
William Ruales <ruales@hotmail.co.uk>
2023-02-13 16:29:20 +0000

    add before merge

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const target = document.getElementById("target")
 const title1 = document.getElementById("title")
 const author1 = document.getElementById("author")
 const pages1 = document.getElementById("pages")
+const exp = document.getElementById("exp")
 
 const myLibrary = [{ title: "this", author: "is", pages: "it" }]
 const book1 = new Books("LOTR", "TOLKIEN", 300)
+const book2 = new Books("Starwars", "G Lucas", 200)
 book1.info()
 myLibrary.push(book1)
+myLibrary.push(book2)
 
 
 function Books(title, author, pages) {
@@ -51,22 +54,51 @@ const newTable = document.createElement("table");
 newTable.innerHTML = "<thead><th>Title</th><th>Author</th> <th></th></thead>";
 target.appendChild(newTable)
 
-for (let book of myLibrary) {
-    setTable(book)
-}
 
 function setTable(x) {
     const newRow = document.createElement("tr");
     const tdPlayer = document.createElement("td");
     const tdScore = document.createElement("td");
+    const delButton = document.createElement("button")
+    delButton.setAttribute("id", "exp")
     tdPlayer.textContent = x.title;
     tdScore.textContent = x.author;
+    delButton.textContent = "Delete"
+
     newRow.appendChild(tdPlayer);
     newRow.appendChild(tdScore);
     newTable.appendChild(newRow);
+    newRow.appendChild(delButton)
     console.log(x)
+
+
 
 }
 
+exp.addEventListener("click", remove)
+
+function remove() {
+    myLibrary.pop()
+    console.log("remove works", myLibrary)
+    var rowCount = newTable.rows.length;
+
+    newTable.deleteRow(rowCount - 1);
+}
+
+
+// function loop() {
+//     for (let book of myLibrary) {
+//         console.log(book, "ping ping")
+//         setTable(book)
+//     }
+// }
+
+
+function tester() {
+    return myLibrary
+}
+console.log(tester())
 
 form.addEventListener("submit", add)
+setTable(book1)
+setTable(book2)
